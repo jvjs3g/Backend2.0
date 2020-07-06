28e920ce4273fd3d4872d49863cda9c703c78173
joao vitor <joao3klb@hotmail.com>
2020-07-05 21:03:19 -0300

    migrations

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,8 @@ import 'express-async-errors';
 import routes from './routes';
 import AppError from './errors/AppError';
 
-import createConnection from './database';
+import './database';
 
-createConnection();
 const app = express();
 
 app.use(express.json());
---
+++ src/database/migrations/1593993289103-CreateCategory.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateCategory1593993289103 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name:'category',
+          columns:[
+            {
+              name:'id',
+              type:'uuid',
+              isPrimary:true,
+              generationStrategy:'uuid'
+            },
+            {
+              name:'title',
+              type:'varchar',
+              isNullable:false
+            },
+            {
+              name:'created_at',
+              type:'timestamp',
+              default:'now()'
+            },
+            {
+              name:'updated_at',
+              type:'timestamp',
+              default:'now()'
+            },
+          ],
+        }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('category');
+    }
+
+}
