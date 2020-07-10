fd0b7977d70e12c51469c2a090d94c3924fad900
joao vitor <joao3klb@hotmail.com>
2020-07-10 19:58:04 -0300

    teste de funções adicionais

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,15 +1,16 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import TransactionsRepository from "../repositories/TransactionsRepository";
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
 
   public async execute(id: string): Promise<void> {
-    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transactionsRepository = getRepository(Transaction);
     await transactionsRepository.delete(id);
     return;
   }
-  
+
 }
 
 export default DeleteTransactionService;
